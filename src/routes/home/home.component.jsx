4bf2c242b733129b81291af76497c9c5c6db94c4
swh <sungwoo166@gmail.com>
2022-07-16 19:59:46 +0900

    nav-bar 디자인 변경
    .

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,7 +1,6 @@
 import React, {Fragment} from 'react';
-import ReactDOM from 'react-dom';
 import ReactFullpage from '@fullpage/react-fullpage';
-import {Link} from "react-router-dom";
+import './home.styles.css'
 
 const Menu = () => (
     <div
@@ -12,14 +11,14 @@ const Menu = () => (
             zIndex: 100,
         }}
     >
-        <ul className="actions">
-            <li>
-                <a href='src/routes/home/home.component#coverPage'> Cover</a>
-                <a href='src/routes/home/home.component#aboutMePage'> About Me</a>
-                <a href='src/routes/home/home.component#skillsPage'> Skills </a>
-                <a href='src/routes/home/home.component#projectPage'> Projects</a>
-                <a href='src/routes/home/home.component#contactPage'> Contact </a>
-            </li>
+        <ul className="actions menu-3">
+
+            <li><a href='#coverPage' className='font-link'> Cover</a></li>
+
+            <li><a href='#aboutMePage' className='font-link'> About Me</a></li>
+            <li><a href='#skillsPage' className='font-link'> Skills </a></li>
+            <li><a href='#projectPage' className='font-link'> Projects</a></li>
+            <li><a href='#contactPage' className='font-link'> Contact </a></li>
         </ul>
     </div>
 );
@@ -28,48 +27,48 @@ const Menu = () => (
 const Home = () => (
     <Fragment>
         <Menu/>
-    <ReactFullpage
-        //fullpage options
-        licenseKey = {' GNU GPL license v3'}
-        scrollingSpeed = {1000} /* Options here */
-        navigation
-        sectionsColor={["#282c34", "#ff5f45", "#0798ec",'#000000','#000000']}
-        anchors={['coverPage', 'aboutMePage', 'skillsPage', 'projectPage','contactPage']}
-        navigationPosition = 'top'
-        render={({ state, fullpageApi }) => {
-            return (
+        <ReactFullpage
+            //fullpage options
+            licenseKey={' GNU GPL license v3'}
+            scrollingSpeed={1000} /* Options here */
+            navigation
+            sectionsColor={["#282c34", "#ff5f45", "#0798ec", '#000000', '#000000']}
+            anchors={['coverPage', 'aboutMePage', 'skillsPage', 'projectPage', 'contactPage']}
+            navigationPosition='top'
+            render={({state, fullpageApi}) => {
+                return (
 
-                <ReactFullpage.Wrapper>
-                    <div className="section">
-                        <h3>coverPage</h3>
-                    </div>
-
-                    <div className="section">
-                        <h3>aboutMePage</h3>
-                    </div>
-
-                    <div className="section">
-                        <h3>skillsPage</h3>
-
-                    </div>
-
-                    <div className="section">
-                        <div className="slide">
-                            <h3>프로젝트 1</h3>
+                    <ReactFullpage.Wrapper>
+                        <div className="section">
+                            <h3>coverPage</h3>
                         </div>
-                        <div className="slide">
-                            <h3>프로젝트 2</h3>
+
+                        <div className="section">
+                            <h3>aboutMePage</h3>
                         </div>
-                    </div>
 
-                    <div className="section">
-                        <h3>contactPage</h3>
-                    </div>
+                        <div className="section">
+                            <h3>skillsPage</h3>
 
-                </ReactFullpage.Wrapper>
-            );
-        }}
-    />
+                        </div>
+
+                        <div className="section">
+                            <div className="slide">
+                                <h3>프로젝트 1</h3>
+                            </div>
+                            <div className="slide">
+                                <h3>프로젝트 2</h3>
+                            </div>
+                        </div>
+
+                        <div className="section">
+                            <h3>contactPage</h3>
+                        </div>
+
+                    </ReactFullpage.Wrapper>
+                );
+            }}
+        />
     </Fragment>
     // return (
     //     <div>
