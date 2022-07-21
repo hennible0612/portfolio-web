2c6e1573a9eb9536264a566a7fc592c6d77fbec2
swh <sungwoo166@gmail.com>
2022-07-22 00:30:59 +0900

    기본틀
    .

--- src/component/project/project.component.jsx
+++ src/component/project/project.component.jsx
@@ -3,37 +3,36 @@ import SimpleImageSlider from "react-simple-image-slider";
 import pay from '../../assets/mangoTang/결제하기.png'
 import login from '../../assets/mangoTang/로그인.png'
 import cart from '../../assets/mangoTang/장바구니.png'
-import home from '../../assets/mangoTang/홈_adobe_express.svg'
 import homee from '../../assets/mangoTang/홈.png'
 
-
+import home from '../../assets/mangoTang/mangoTangHome.svg'
+import {ReactComponent as GitHubIcon} from "../../assets/github.svg";
+import {ReactComponent as WebIcon} from "../../assets/web.svg";
 
 const Project = () => {
-    const images = [
-        pay,
-        login,
-        cart,
-        homee,
-    ];
+    // const images = [
+    //     pay,
+    //     login,
+    //     cart,
+    //     homee,
+    // ];
     return (
 
         <article className='article '>
             <div className='project-container'>
-                {/*<div className='slider-div'>*/}
-                {/*        <SimpleImageSlider className='rsis-container'*/}
-                {/*            width='30vw'*/}
-                {/*            height='40vw'*/}
-                {/*            images={images}*/}
-                {/*            showBullets={true}*/}
-                {/*            showNavs={true}*/}
-
-                {/*        />*/}
+                <div className='project-img-link-container'>
                     <img className='project-img' src={home}/>
-                {/*</div>*/}
-                <div className='info-div'>
-                    2
-                </div>
+                    <div className='link-div'>
+                        <GitHubIcon className='shopping-icon'/>
+                        <WebIcon className='shopping-icon'/>
 
+                    </div>
+
+                </div>
+            </div>
+
+            <div className='info-div'>
+                2
             </div>
 
 
