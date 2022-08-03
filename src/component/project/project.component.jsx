c463a481696a2128fc27a5b5f6cfe16c04fe2ac0
swh <sungwoo166@gmail.com>
2022-08-03 16:30:45 +0900

    기본 설명
    .

--- src/component/project/project.component.jsx
+++ src/component/project/project.component.jsx
@@ -23,19 +23,55 @@ const Project = () => {
 
             <div className='project-container'>
                 <div className='project-title-container'>
-                    something
+                    <text><b>망고땡 쇼핑몰</b></text>
                 </div>
 
                 <div className='project-info-container'>
                     <div className="image-link-container">
                         <div className="image-item">
-
+                            <img className='project-img' src={home}/>
                         </div>
                         <div className="link-item">
+                            <GitHubIcon onClick={openGithub} className='shopping-icon'/>
+                            <WebIcon className='shopping-icon'/>
                         </div>
                     </div>
                     <div className="project-summery-container">
+                        <div className="skill-item">
+                            <text className='skills'>
+                                DJANGO
+                            </text>
+                            <text className='skills'>
+                                HTML/CSS/JS
+                            </text>
+                            <text className='skills'>
+                                NGINX GUNICORN
+                            </text>
+                            <text className='skills'>
+                                AMAZON LIGHTSAIL
+                            </text>
+                        </div>
                         <div className="summery-item">
+                            <div className="summery">
+                                {/*<p>처음으로 만든 프로젝트입니다. 프론트엔드 부터 백엔드까지의 경험을 하고 싶어서 쇼핑몰을 선택했습니다.</p>*/}
+                                <b>기본적인 쇼핑몰입니다. 상품 목록, 결제, 환불 등 쇼핑몰에서 지원하는 기능들을 구현했습니다.</b>
+                                <br></br><br></br>
+                                <text>Framework는 Django를 사용하여 MVT 모델을 익혔습니다. 또한 자바스크립트에 익숙해지고자 바닐라 자바스크립트를 사용했습니다.
+                                </text>
+                                <text>
+                                    Iamport api를 사용하여 결제, 환불 기능을 구현하였고, sweettracker api를 사용하여 실시간 택배 추적 그리고
+                                    daum address api 를 사용하여 주소를 입력받았습니다.
+                                </text>
+                                <text>
+                                    Django-allauth를 사용하여 네이버, 구글, 카카오 sns 회원가입, 로그인 기능을 구현했습니다.
+                                </text>
+                                <text>
+                                    mazon Lightsail, Ngnix, Gunicorn을 사용하여 배포하였습니다.
+                                </text>
+                                <hr></hr>
+                                <text> </text>
+
+                            </div>
                         </div>
                     </div>
                 </div>
